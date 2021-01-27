3f4e8d2e5c23987ca27f2c1996c30eab0f212393
Chika Onuchukwu <chuzksy@gmail.com>
2021-01-27 09:38:33 +0100

    - Add Jwt authentication - Add Roles to User

--- JwtRefreshTokens/Controllers/UserController.cs
+++ JwtRefreshTokens/Controllers/UserController.cs
@@ -22,5 +22,21 @@ namespace JwtRefreshTokens.Controllers
 
             return Ok(result);
         }
+
+        [HttpPost("token")]
+        public async Task<IActionResult> GetTokenAsync(TokenRequestModel model)
+        {
+            var result = await _userService.GetTokenAsync(model);
+
+            return Ok(result);
+        }
+
+        [HttpPost("addrole")]
+        public async Task<IActionResult> AddRoleAsync(AddRoleModel model)
+        {
+            var result = await _userService.AddRoleAsync(model);
+
+            return Ok(result);
+        }
     }
 }
--- JwtRefreshTokens/Services/IUserService.cs
+++ JwtRefreshTokens/Services/IUserService.cs
@@ -6,5 +6,7 @@ namespace JwtRefreshTokens.Services
     public interface IUserService
     {
         Task<string> RegisterAsync(RegisterModel model);
+        Task<AuthenticationModel> GetTokenAsync(TokenRequestModel model);
+        Task<string> AddRoleAsync(AddRoleModel model);
     }
 }
