e32f346b12b377222093c5b606ba3b99b3cd1926
Chika Onuchukwu <chuzksy@gmail.com>
2021-01-27 04:26:06 +0100

    Add all packages

---
+++ JwtRefreshTokens/Contexts/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using JwtRefreshTokens.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace JwtRefreshTokens.Contexts
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) 
+            : base(options)
+        {
+
+        }
+    }
+}
