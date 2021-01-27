3bf037e8ef2de25cd31900679e6d9a212f7f7290
Chika Onuchukwu <chuzksy@gmail.com>
2021-01-27 04:58:20 +0100

    - Remove ignored files - Add application dbcontext seed data

---
+++ JwtRefreshTokens/Constants/Authorization.cs
@@ -0,0 +1,16 @@
+﻿namespace JwtRefreshTokens.Constants
+{
+    public class Authorization
+    {
+        public enum Roles
+        {
+            Administrator,
+            Moderator,
+            User
+        }
+        public const string default_username = "user";
+        public const string default_email = "chuzksy@yahoo.com";
+        public const string default_password = "Pa$$w0rd.";
+        public const Roles default_role = Roles.User;
+    }
+}
